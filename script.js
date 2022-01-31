f10bb82ab7127a8220f318d241ee5aff21cc315e
farihakausar <88570666+farihakausar@users.noreply.github.com>
2022-01-31 16:51:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+(function () {
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+
+  
+  var firstLetter = names[i].charAt(0).toLowerCase();
+  
+  
+   if (firstLetter=='j') {
+    console.log("helloSpeaker"+(names[i]));
+   
+    } else {
+        console.log("byeSpeaker"+(names[i]));
+    }
+}
+})();
